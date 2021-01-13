458b332f77dda6ae9a2e7287d1b90b6fca8aa21e
Paul Tansley <pt360@exeter.ac.uk>
2021-01-13 11:20:11 +0000

    Changed to work with included data

--- R/Transfer function.R
+++ R/Transfer function.R
@@ -1,6 +1,5 @@
 
-test_trans <- function(test_data, 
-                       tf_file = "Eurowood.csv", 
+test_trans <- function(test_data,
                        region_name = F){
 require(rioja)
 require(tidyverse)
@@ -12,7 +11,7 @@ mydir <- paste0(test_data, "/")
 if(!dir.exists(mydir)) dir.create(mydir)
 name <- test_data
 test_data <- read.csv(paste0(test_data, ".csv"))
-EuroTF <- read.csv(tf_file)
+EuroTF <- data(Eurowood)
 region_name <- region_name
 
 
@@ -23,11 +22,11 @@ EuroTF <-if(region_name == F){EuroTF}
 #Extract species from euro tf
 Spec <- EuroTF[,9:55]
 
-#Extract coulmns that have 0 in every row 
+#Extract coulmns that have 0 in every row
 Spec <-  Spec[, colSums(Spec != 0) > 0]
 
 #Extract WTD
-WT <- EuroTF$WTD  
+WT <- EuroTF$WTD
 
 #Run Model
 EuroTF_model <- WA(Spec, WT, tolDW = T)
@@ -36,20 +35,20 @@ EuroTF_model.cv <- crossval(EuroTF_model, cv.method="loo")
 #Check performance
 print(performance(EuroTF_model.cv))
 
-perf <- as.data.frame(do.call(rbind,performance(EuroTF_model.cv))) 
+perf <- as.data.frame(do.call(rbind,performance(EuroTF_model.cv)))
 perf <- perf[-1,]
 
 write.csv(perf, file =paste0(name, "/", name,"_performance.csv"))
 
-#Run model on data 
+#Run model on data
 EuroTF_recon <- predict(EuroTF_model.cv, test_data, sse=TRUE, nboot=1000)
 
-#Create data 
+#Create data
 recon <- data.frame(EuroTF_recon$fit.boot)
 error <- EuroTF_recon$SEP.boot
 z <- standardize(recon)
 
-write.csv(recon, file = paste0(name,"/",name, "_reconstruction.csv")) 
+write.csv(recon, file = paste0(name,"/",name, "_reconstruction.csv"))
 
 write.csv(error, file = paste0(name,"/",name, "_error.csv"))
 
